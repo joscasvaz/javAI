7b48d12113cd56044f68265455eec871fbd5fa86
joscasvaz <jicastrotic99@gmail.com>
2023-03-22 18:11:42 +0100

    feat: reading and testing relu

--- src/test/utils/TestUtils.java
+++ src/test/utils/TestUtils.java
@@ -1,8 +1,22 @@
 package test.utils;
 
+import java.util.List;
+
+import utils.CommonFunctions;
+import utils.Parsers;
+import utils.Readers;
+
 public class TestUtils {
 	
+	private static List<List<Double>> data = Readers.readCSV("/javAI/src/test/data/inputs/utils.csv",
+			Parsers.parseToDouble);
+	
 	public static void main(String[] args) {
+		
+		for(List<Double> l:data)
+			for(Double d:l)
+				System.out.println(CommonFunctions.relu.applyAsDouble(d));
+			
 		
 	}
 }
