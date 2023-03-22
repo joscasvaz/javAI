f6ce3d82c005c1cb5d3445bc39a982d0c63c1685
joscasvaz <jicastrotic99@gmail.com>
2023-03-22 18:10:33 +0100

    fix: public static

--- src/utils/CommonFunctions.java
+++ src/utils/CommonFunctions.java
@@ -4,10 +4,9 @@ import java.util.function.DoubleUnaryOperator;
 
 public class CommonFunctions {
 	
-	public DoubleUnaryOperator threshold = x->{ return 0 < x ? 1 : 0; };
-	public DoubleUnaryOperator relu = x->{ return 0 < x ? x : 0; };
-	public DoubleUnaryOperator signo = x-> {return threshold.applyAsDouble(x) < 1 ? -1 : 1; };
-	public DoubleUnaryOperator sigmoid = x ->{
-		return Math.pow(1 + Math.pow(Math.E, -x), -1); };
+	public static DoubleUnaryOperator threshold = x->{ return 0 < x ? 1 : 0; };
+	public static DoubleUnaryOperator relu = x->{ return 0 < x ? x : 0; };
+	public static DoubleUnaryOperator signo = x-> {return threshold.applyAsDouble(x) < 1 ? -1 : 1; };
+	public static DoubleUnaryOperator sigmoid = x ->{ return Math.pow(1 + Math.pow(Math.E, -x), -1); };
 	
 }
