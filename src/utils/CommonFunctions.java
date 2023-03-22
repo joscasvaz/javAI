222ee05b75c4bd376fc47a7c36c1203fc19f8f14
joscasvaz <jicastrotic99@gmail.com>
2023-03-22 18:52:30 +0100

    fix: less dependencies

--- src/utils/CommonFunctions.java
+++ src/utils/CommonFunctions.java
@@ -6,7 +6,7 @@ public class CommonFunctions {
 	
 	public static DoubleUnaryOperator threshold = x->{ return 0 < x ? 1 : 0; };
 	public static DoubleUnaryOperator relu = x->{ return 0 < x ? x : 0; };
-	public static DoubleUnaryOperator signo = x-> {return threshold.applyAsDouble(x) < 1 ? -1 : 1; };
+	public static DoubleUnaryOperator signo = x-> {return x < 0 ? -1 : 1; };
 	public static DoubleUnaryOperator sigmoid = x ->{ return Math.pow(1 + Math.pow(Math.E, -x), -1); };
 	
 }
