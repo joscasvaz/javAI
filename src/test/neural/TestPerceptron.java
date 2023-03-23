3487c55faf7106744fe9189ea0b4b58e276b88ab
joscasvaz <jicastrotic99@gmail.com>
2023-03-23 18:11:15 +0100

    fix: parseToDouble

--- src/test/neural/TestPerceptron.java
+++ src/test/neural/TestPerceptron.java
@@ -1,8 +1,34 @@
 package test.neural;
 
+import java.util.List;
+
+import neural.Perceptron;
+import utils.CommonFunctions;
+import utils.Parsers;
+import utils.Readers;
+
 public class TestPerceptron {
 	
+	 private static String inputsPerceptron = "../javAI/src/test/data/inputs/perceptron.csv";
+		
+		private static List<List<Double>> data = Readers.readCSV(inputsPerceptron,
+				Parsers.parseToDouble);
+	
 	public static void main(String[] args) {
+		
+		List<Double> weights = data.get(0);
+		List<List<Double>> values = data.subList(1, data.size());
+		
+		for(List<Double> l:values) {
+			
+			try {
+				
+				Perceptron p1 = new Perceptron(l, weights, null, CommonFunctions.threshold);
+				System.out.println(String.format("Weights: %s\nValues: %s\nOutput: %s\n#",
+						weights, l, p1.getOutput()));
+				
+			} catch (Exception e) { e.printStackTrace(); }
+		}
 		
 	}
 }
