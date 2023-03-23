d636c6943a7f7d91f0a07ad02f1945f01a32b93e
joscasvaz <jicastrotic99@gmail.com>
2023-03-23 18:24:37 +0100

    feat: tanh
    Other minor changes added.

--- src/utils/CommonFunctions.java
+++ src/utils/CommonFunctions.java
@@ -4,9 +4,12 @@ import java.util.function.DoubleUnaryOperator;
 
 public class CommonFunctions {
 	
-	public static DoubleUnaryOperator threshold = x->{ return 0 < x ? 1 : 0; };
-	public static DoubleUnaryOperator relu = x->{ return 0 < x ? x : 0; };
-	public static DoubleUnaryOperator sig = x-> {return x < 0 ? -1 : 1; };
-	public static DoubleUnaryOperator sigmoid = x ->{ return 1 / (1 + Math.pow(Math.E, -x)); };
+	public static DoubleUnaryOperator threshold = z->{ return 0 < z ? 1 : 0; };
+	public static DoubleUnaryOperator relu = z->{ return 0 < z ? z : 0; };
+	public static DoubleUnaryOperator sig = z-> {return z < 0 ? -1 : 1; };
+	public static DoubleUnaryOperator sigmoid = z ->{ return 1 / (1 + Math.pow(Math.E, -z)); };
+	public static DoubleUnaryOperator tanh = z ->{
+		Double ez = Math.pow(Math.E, z);
+		return (ez - 1 / ez) / (ez + 1 / ez); };
 	
 }
