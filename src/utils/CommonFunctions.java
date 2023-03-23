3d6d78fe56d34bb73102ad88d86c0ef042210594
joscasvaz <jicastrotic99@gmail.com>
2023-03-23 18:19:59 +0100

    fix: simplify sigmoid

--- src/utils/CommonFunctions.java
+++ src/utils/CommonFunctions.java
@@ -7,6 +7,6 @@ public class CommonFunctions {
 	public static DoubleUnaryOperator threshold = x->{ return 0 < x ? 1 : 0; };
 	public static DoubleUnaryOperator relu = x->{ return 0 < x ? x : 0; };
 	public static DoubleUnaryOperator signo = x-> {return x < 0 ? -1 : 1; };
-	public static DoubleUnaryOperator sigmoid = x ->{ return Math.pow(1 + Math.pow(Math.E, -x), -1); };
+	public static DoubleUnaryOperator sigmoid = x ->{ return 1 / (1 + Math.pow(Math.E, -x)); };
 	
 }
